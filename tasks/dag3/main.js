d72627df36b64de57298ee7d2d4d65fbc20122c0
Marcus <mcihme@gmail.com>
2025-08-19 15:02:53 +0200

    Updaed main to be beautiful

--- tasks/dag3/main.js
+++ tasks/dag3/main.js
@@ -1,20 +1,25 @@
 var size_x = 500;
 var size_y = 500;
 
+var rects_x = 100;
+var rects_y = 100;
+
+let base_x = size_x / (rects_x + 2);
+let base_y = size_y / (rects_y + 2);
+
+let color_factor_x = 255 / rects_x;
+let color_factor_y = 255 / rects_y;
+
 function setup() {
     createCanvas(size_x, size_y);
     background(255);
+    strokeWeight(0);
 }
 
 function draw() {
-    let base_x = size_x / 12;
-    let base_y = size_y / 12;
-
-    let color_factor = 255 / 10;
-
-    for(let i = 0; i < 10; i++) {
-        for(let j = 0; j < 10; j++) {
-            fill(color(j * color_factor, i * color_factor, 0));
+    for(let i = 0; i < rects_x; i++) {
+        for(let j = 0; j < rects_y; j++) {
+            fill(color(j * color_factor_y, i * color_factor_x, 0));
             rect(base_x + base_x*i, base_y + base_y*j, base_x, base_y)
         }
     }
